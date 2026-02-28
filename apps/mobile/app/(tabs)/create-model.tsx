6b440d6b1adf37a7a28582a15c522efde2d2300d
Jxnatan7 <jonatan.santos.contato@gmail.com>
2026-02-28 00:39:04 -0300

    add useDebounce in create model text input

--- apps/mobile/app/(tabs)/create-model.tsx
+++ apps/mobile/app/(tabs)/create-model.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useCallback, useRef } from "react";
-import { StyleSheet, Switch, TextInput as RNTextInput } from "react-native";
+import React, { useState, useCallback } from "react";
+import { StyleSheet, Switch } from "react-native";
 import { Redirect } from "expo-router";
 import { useTheme } from "@shopify/restyle";
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
@@ -15,12 +15,159 @@ import useLoginModal from "@/hooks/useLoginModal";
 import { Theme } from "@/theme";
 import { LinearGradient } from "expo-linear-gradient";
 import { LiquidGlassView } from "@/components/theme/LiquidGlassView";
+import { useDebounce } from "@/hooks/useDebounce";
+
+const EmptyState = ({ pickImageFromGallery, takePhoto, theme }: any) => (
+  <>
+    <Text variant="containerHeader" mt="xxxl" fontSize={22} textAlign="center">
+      Tire uma foto ou {"\n"} escolha uma da galeria
+    </Text>
+    <Text
+      fontFamily="MulishFont"
+      color="gray300"
+      mt="m"
+      fontSize={16}
+      textAlign="center"
+    >
+      Faça upload de uma imagem {"\n"} para transformá-la em um modelo 3D
+    </Text>
+
+    <Box style={styles.buttonContainer}>
+      <IconButton
+        variant="imageOption"
+        text="Enviar"
+        // onPress={() => push("/model-preview")}
+        onPress={pickImageFromGallery}
+        icon={
+          <Box borderColor="blue300" marginBottom="s">
+            <FontAwesome name="photo" size={40} color={theme.colors.blue300} />
+          </Box>
+        }
+        flexDirection="column-reverse"
+      />
+
+      <IconButton
+        variant="imageOption"
+        text="Tirar Foto"
+        onPress={takePhoto}
+        icon={
+          <Box borderColor="blue300" marginBottom="s">
+            <FontAwesome name="camera" size={40} color={theme.colors.pink100} />
+          </Box>
+        }
+        flexDirection="column-reverse"
+      />
+    </Box>
+  </>
+);
+
+const FormState = ({
+  selectedImage,
+  name,
+  setName,
+  isPublic,
+  handlePublicToggle,
+  onGenerate,
+  isButtonDisabled,
+  reset,
+}: any) => (
+  <Box alignItems="center" width="100%" flex={1}>
+    <Box style={styles.imageContainer}>
+      <Image
+        source={{ uri: selectedImage! }}
+        width="100%"
+        height="100%"
+        contentFit="cover"
+        paddingBottom="l"
+        borderRadius={16}
+      />
+      <LinearGradient
+        colors={["#1A1A2E", "rgba(8, 8, 8, 0)"]}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 0, y: 0 }}
+        style={styles.gradient}
+      />
+    </Box>
+
+    <Box width="95%" marginTop="xl" padding="m" borderRadius={16}>
+      <LiquidGlassView style={StyleSheet.absoluteFill} />
+
+      <TextInput
+        placeholder="Nome do Modelo"
+        value={name ? name : undefined}
+        onChangeText={setName}
+        placeholderTextColor="#FFF"
+        backgroundColor="transparent"
+        color="white"
+        style={{ fontFamily: "Sekuya-Regular", fontSize: 24, padding: 0 }}
+        containerProps={{
+          style: {
+            borderBottomWidth: 1,
+            borderBottomColor: "#FFF",
+          },
+        }}
+      />
+
+      <Box
+        style={styles.inputContainer}
+        flexDirection="row"
+        alignItems="center"
+        gap="m"
+      >
+        <Switch value={isPublic} onValueChange={handlePublicToggle} />
+        <Text variant="body">Público (visível para todos)</Text>
+      </Box>
+
+      <IconButton
+        icon={
+          <MaterialCommunityIcons name="creation" size={24} color="black" />
+        }
+        width="100%"
+        marginBottom="m"
+        onPress={onGenerate}
+        disabled={isButtonDisabled}
+        flexDirection="row-reverse"
+        gap="m"
+        maxWidth={500}
+        text="Gerar Modelo 3D"
+        textProps={{
+          color: "black",
+          fontFamily: "MulishFontBold",
+          fontSize: 18,
+        }}
+        style={{ opacity: isButtonDisabled ? 0.7 : 1 }}
+        glassProps={{
+          tint: "light",
+          intensity: 50,
+        }}
+      />
+
+      <IconButton
+        icon={<MaterialCommunityIcons name="image" size={24} color="black" />}
+        width="100%"
+        onPress={reset}
+        flexDirection="row-reverse"
+        gap="m"
+        maxWidth={500}
+        backgroundColor="yellow100"
+        text="Escolher outra imagem"
+        textProps={{
+          color: "extraBlack",
+          fontFamily: "MulishFontBold",
+          fontSize: 18,
+        }}
+        glassProps={{
+          intensity: 50,
+        }}
+      />
+    </Box>
+  </Box>
+);
 
 export default function CreateModel() {
   const theme = useTheme<Theme>();
   const { isAuthenticated } = useAuthStore();
   const { isGenerating } = useModelContext();
-  const inputRef = useRef<RNTextInput>(null);
 
   const {
     name,
@@ -36,6 +183,8 @@ export default function CreateModel() {
   const { showModal } = useLoginModal(
     "Para criar um modelo público, você precisa estar logado.",
   );
+
+  const { debouncedFn, clear } = useDebounce(setName, 1000);
 
   const [isPublic, setIsPublic] = useState(true);
 
@@ -58,159 +207,6 @@ export default function CreateModel() {
     return <Redirect href="/model-preview" />;
   }
 
-  const renderEmptyState = () => (
-    <>
-      <Text
-        variant="containerHeader"
-        mt="xxxl"
-        fontSize={22}
-        textAlign="center"
-      >
-        Tire uma foto ou {"\n"} escolha uma da galeria
-      </Text>
-      <Text
-        fontFamily="MulishFont"
-        color="gray300"
-        mt="m"
-        fontSize={16}
-        textAlign="center"
-      >
-        Faça upload de uma imagem {"\n"} para transformá-la em um modelo 3D
-      </Text>
-
-      <Box style={styles.buttonContainer}>
-        <IconButton
-          variant="imageOption"
-          text="Enviar"
-          // onPress={() => push("/model-preview")}
-          onPress={pickImageFromGallery}
-          icon={
-            <Box borderColor="blue300" marginBottom="s">
-              <FontAwesome
-                name="photo"
-                size={40}
-                color={theme.colors.blue300}
-              />
-            </Box>
-          }
-          flexDirection="column-reverse"
-        />
-
-        <IconButton
-          variant="imageOption"
-          text="Tirar Foto"
-          onPress={takePhoto}
-          icon={
-            <Box borderColor="blue300" marginBottom="s">
-              <FontAwesome
-                name="camera"
-                size={40}
-                color={theme.colors.pink100}
-              />
-            </Box>
-          }
-          flexDirection="column-reverse"
-        />
-      </Box>
-    </>
-  );
-
-  const renderFormState = () => (
-    <Box alignItems="center" width="100%" flex={1}>
-      <Box style={styles.imageContainer}>
-        <Image
-          source={{ uri: selectedImage! }}
-          width="100%"
-          height="100%"
-          contentFit="cover"
-          paddingBottom="l"
-          borderRadius={16}
-        />
-        <LinearGradient
-          colors={["#1A1A2E", "rgba(8, 8, 8, 0)"]}
-          start={{ x: 0, y: 1 }}
-          end={{ x: 0, y: 0 }}
-          style={styles.gradient}
-        />
-      </Box>
-
-      <Box width="95%" marginTop="xl" padding="m" borderRadius={16}>
-        <LiquidGlassView style={StyleSheet.absoluteFill} />
-
-        <TextInput
-          placeholder="Nome do Modelo"
-          value={name ? name : undefined}
-          onEndEditing={(event) => {
-            setName(event.nativeEvent.text);
-          }}
-          placeholderTextColor="#FFF"
-          backgroundColor="transparent"
-          color="white"
-          style={{ fontFamily: "Sekuya-Regular", fontSize: 24, padding: 0 }}
-          containerProps={{
-            style: {
-              borderBottomWidth: 1,
-              borderBottomColor: "#FFF",
-            },
-          }}
-        />
-
-        <Box
-          style={styles.inputContainer}
-          flexDirection="row"
-          alignItems="center"
-          gap="m"
-        >
-          <Switch value={isPublic} onValueChange={handlePublicToggle} />
-          <Text variant="body">Público (visível para todos)</Text>
-        </Box>
-
-        <IconButton
-          icon={
-            <MaterialCommunityIcons name="creation" size={24} color="black" />
-          }
-          width="100%"
-          marginBottom="m"
-          onPress={onGenerate}
-          disabled={isButtonDisabled}
-          flexDirection="row-reverse"
-          gap="m"
-          maxWidth={500}
-          text="Gerar Modelo 3D"
-          textProps={{
-            color: "black",
-            fontFamily: "MulishFontBold",
-            fontSize: 18,
-          }}
-          style={{ opacity: isButtonDisabled ? 0.7 : 1 }}
-          glassProps={{
-            tint: "light",
-            intensity: 50,
-          }}
-        />
-
-        <IconButton
-          icon={<MaterialCommunityIcons name="image" size={24} color="black" />}
-          width="100%"
-          onPress={reset}
-          flexDirection="row-reverse"
-          gap="m"
-          maxWidth={500}
-          backgroundColor="yellow100"
-          text="Escolher outra imagem"
-          textProps={{
-            color: "extraBlack",
-            fontFamily: "MulishFontBold",
-            fontSize: 18,
-          }}
-          glassProps={{
-            intensity: 50,
-          }}
-        />
-      </Box>
-    </Box>
-  );
-
   return (
     <Container
       variant="screen"
@@ -222,7 +218,24 @@ export default function CreateModel() {
       ]}
       hideHeader
     >
-      {selectedImage ? renderFormState() : renderEmptyState()}
+      {selectedImage ? (
+        <FormState
+          selectedImage={selectedImage}
+          name={name}
+          setName={debouncedFn}
+          isPublic={isPublic}
+          handlePublicToggle={handlePublicToggle}
+          onGenerate={onGenerate}
+          isButtonDisabled={isButtonDisabled}
+          reset={reset}
+        />
+      ) : (
+        <EmptyState
+          pickImageFromGallery={pickImageFromGallery}
+          takePhoto={takePhoto}
+          theme={theme}
+        />
+      )}{" "}
     </Container>
   );
 }
