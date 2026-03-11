bc89b07209621e5709082bb51eee02a0b332c14a
Jonatan Gabriel <jgabrieldesouzasantos@gmail.com>
2026-03-11 20:58:19 -0300

    Excluir index.tsx

--- apps/mobile/components/theme/MessageList/index.tsx
+++
@@ -1,8 +0,0 @@
-import { RestyleFlashListProps } from "@/components/restyle";
-import { FlashList } from "../FlashList";
-
-export type MessageListProps = RestyleFlashListProps;
-
-export const MessageList = ({ ...props }: MessageListProps) => {
-  return <FlashList {...props} variant="messages" />;
-};
